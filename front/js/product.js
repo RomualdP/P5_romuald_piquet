9c1ddf726b47077fc10d4f3a13113146d9bc6315
RomualdP <romuald.piquet@gmail.com>
2022-03-15 11:35:31 +0100

    addToCart function to localStorage

--- front/js/product.js
+++ front/js/product.js
@@ -1,40 +1,82 @@
+import { productList } from "./script.js";
+import { save, load } from "./localStorage.js"
+
+// récupération de l'ID du produit via l'URL
 let str = window.location.href;
 let url = new URL(str);
 let id = url.searchParams.get("id");
 
-
-async function main() {
-    const product = await productSpecList();
+// fonction qui récupère les éléments de la promesse et qui execute la fonction d'affichage et auto executée
+(async function () {
+    const product = await productList("http://localhost:3000/api/products/"+ id);
     displaySpec(product)   
-  }
+  }) ()
 
 
+// // fonction qui retourne la promesse de l'API
+// function productSpecList () {
+//     return fetch("http://localhost:3000/api/products/"+ id )
+//       .then(function(res) {
+//         if (res.ok) {
+//           return res.json();
+// // On retourne la réponse au format json
+//         }
+//       })
+//       .then(function(product) {
+//         return product
+// // On récupère les produits de la production list
+//       })
+//       .catch(function(err) {
+//         // Une erreur est survenue
+//       });
+// }
 
-function productSpecList () {
-    return fetch("http://localhost:3000/api/products/"+ id )
-      .then(function(res) {
-        if (res.ok) {
-          return res.json();
-// On retourne la réponse au format json
-        }
-      })
-      .then(function(product) {
-        return product
-// On récupère les produits de la production list
-      })
-      .catch(function(err) {
-        // Une erreur est survenue
-      });
-}
 
+// Fonction qui injecte les éléments dans la page html 
 function displaySpec (product) {
     document.querySelector(".item__img").innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
     document.getElementById("title").textContent = product.name
     document.getElementById("price").textContent = product.price
     document.getElementById("description").textContent = product.description
-    for (color in product.colors ) {
+    for (let color in product.colors ) {
         document.getElementById("colors").innerHTML += `<option value="${product.colors[color]}">${product.colors[color]}</option>`
     }
+    
 }
 
-main ()
+
+let addToCartBtn = document.getElementById("addToCart")
+addToCartBtn.addEventListener('click', addProduct)
+
+function addProduct() {  
+  // if (!load("allProducts")) return
+  let existingProducts = load("allProducts");
+  if (existingProducts == null) existingProducts = [];
+  let productID = id;
+  let productColor = document.getElementById("colors").value;
+  let productQuantity = parseInt(document.getElementById("quantity").value);
+  let product = {
+      "ID": productID,
+      "Color": productColor,
+      "Quantity": productQuantity
+  };
+  if ( product.Quantity > 0 && product.Color != null) {
+    let existingProduct = existingProducts.find(prod => prod.ID === productID && prod.Color === productColor)
+    let existingProductIndex = existingProducts.findIndex(prod => prod.ID === productID && prod.Color === productColor)
+
+    if (existingProduct) {
+      existingProducts[existingProductIndex].Quantity += productQuantity
+    } else {
+      existingProducts.push(product);
+    }
+  }
+  console.log(existingProducts)
+  // console.log(existingProducts)
+  // localStorage.setItem("allProducts", JSON.stringify(existingProducts));
+  save ("allProducts", existingProducts)
+};
+
+// function storageReset () {
+//   localStorage.clear()
+// }
+// storageReset()
--- front/js/script.js
+++ front/js/script.js
@@ -1,33 +1,41 @@
 async function main() {
-  const products = await productList();
-  for (product of products) {
+  const products = await productList("http://localhost:3000/api/products");
+  for (let product of products) {
     displayProducts(product);
   }
 }
 
-function productList() {
-  return fetch("http://localhost:3000/api/products")
-    .then(function (res) {
-      if (res.ok) {
-        return res.json();
-        // On retourne la réponse au format json
-      }
-    })
-    .then(function (products) {
-      return products;
-      // On récupère les produits de la production list
-    })
-    .catch(function (err) {
-      // Une erreur est survenue
-    });
+
+export async function productList (url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
 }
+
+// productList ()
+// function productList() {
+//   return fetch("http://localhost:3000/api/products")
+//     .then(function (res) {
+//       if (res.ok) {
+//         return res.json();
+//         // On retourne la réponse au format json
+//       }
+//     })
+//     .then(function (products) {
+//       return products;
+//       // On récupère les produits de la production list
+//     })
+//     .catch(function (err) {
+//       // Une erreur est survenue
+//     });
+// }
 
 // Pour chaque produit de la liste, on crée un bloc HTML avec chacune des variables
 
-function displayProducts() {
-  document.getElementById(
-    "items"
-  ).innerHTML += `<a href="./product.html?id=${product._id}">
+function displayProducts(product) {
+  if (!document.getElementById("items")) return
+  document.getElementById("items").innerHTML += 
+  `<a href="./product.html?id=${product._id}">
           <article>
             <img src="${product.imageUrl} " alt="${product.altTxt}"/>
             <h3 class="productName">${product.name}</h3>
